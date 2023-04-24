585f7b44d0709ab1c405b571a8708f880c1766d5
Oscar Spencer <oscarspencer@Oscars-MacBook-Air.local>
2023-04-23 23:31:49 -0600

    full working project, MVP is there

---
+++ server.js
@@ -0,0 +1,39 @@
+// server
+const PORT = 8000;
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const API_KEY = "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";
+
+app.listen(PORT, () => {
+  console.log(`Server listening for requests on port ${PORT}`);
+});
+
+app.post("/completions", async (req, res) => {
+  const config = {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: req.body.message }],
+      max_tokens: 100, // length of req message
+    }),
+  };
+
+  try {
+    const response = await fetch(
+      "https://api.openai.com/v1/chat/completions",
+      config
+    );
+    const data = await response.json();
+    res.send(data);
+  } catch (error) {
+    console.log(error);
+  }
+});
--- src/App.js
+++ src/App.js
@@ -1,25 +1,130 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+  const [value, setValue] = useState(null);
+  const [message, setMessage] = useState(null);
+  const [previousChats, setPreviousChats] = useState([]);
+  const [currentTitle, setCurrentTitle] = useState(null);
+
+  const createNewChat = () => {
+    setMessage(null);
+    setValue("");
+    setCurrentTitle(null);
+  };
+
+  const handleClick = (uniqueTitle) => {
+    setCurrentTitle(uniqueTitle);
+    setMessage(null);
+    setValue("");
+  };
+
+  const getMessage = async () => {
+    const config = {
+      method: "POST",
+      body: JSON.stringify({
+        message: value,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    try {
+      const response = await fetch(
+        "http://localhost:8000/completions/",
+        config
+      );
+      const data = await response.json();
+      console.log(data);
+      setMessage(data.choices[0].message);
+    } catch (error) {
+      console.error(error + " NOOOOO!");
+    }
+  };
+
+  useEffect(() => {
+    console.log(currentTitle, value, message);
+    if (!currentTitle && value && message) {
+      setCurrentTitle(value);
+    }
+    if (currentTitle && value && message) {
+      setPreviousChats((prevChats) => [
+        ...prevChats,
+        { title: currentTitle, role: "user", content: value },
+        { title: currentTitle, role: message.role, content: message.content },
+      ]);
+    }
+  }, [message, currentTitle]);
+
+  console.log(previousChats);
+
+  const currentChat = previousChats.filter(
+    (previousChat) => previousChat.title === currentTitle
+  );
+  const uniqueTitles = Array.from(
+    new Set(previousChats.map((previousChat) => previousChat.title))
+  );
+
+  console.log(uniqueTitles);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="sidebar">
+        <button className="new-chat" onClick={createNewChat}>
+          + New Chat
+        </button>
+        <ul className="history">
+          {uniqueTitles?.map((uniqueTitle, index) => {
+            return (
+              <li key={index} onClick={() => handleClick(uniqueTitle)}>
+                {uniqueTitle}
+              </li>
+            );
+          })}
+        </ul>
+        <nav>
+          <p className="name">Made by Oscar</p>
+        </nav>
+      </section>
+      <section className="main">
+        <h1>cloneGPT</h1>
+        <ul className="feed">
+          {currentChat?.map((chatMessage, index) => {
+            return (
+              <li key={index}>
+                <p className="role">{chatMessage.role}</p>
+                <p>{chatMessage.content}</p>
+              </li>
+            );
+          })}
+        </ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input
+              value={value}
+              onChange={(e) => {
+                setValue(e.target.value);
+              }}
+            />
+            <div
+              id="submit"
+              onClick={getMessage}
+              onKeyDown={(event) => {
+                if (event.key === "Enter") {
+                  console.log("NODFDOFIFO");
+                  getMessage();
+                }
+              }}
+            >
+              ➢
+            </div>
+          </div>
+          <p className="info">
+            This is a nice and chill chatGPT clone. No copyright involved. Enjoy.
+          </p>
+        </div>
+      </section>
     </div>
   );
-}
+};
 
 export default App;
